731d696fa1ec7facca51a897b7ba23672fcb973c
Thib <thibaud.berdin@groupeberdin.com>
2020-10-20 13:53:52 +0200

    Refonte avec generateur express

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # airBnbProject
+# Express Js
--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req,res) => {
-res.end('Le Serveur tourne'); 
-});
-
-server.listen(process.env.PORT || 3000);
